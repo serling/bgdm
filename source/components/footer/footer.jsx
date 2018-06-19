2882ae39d35919b3b99c3f200fd765f68dfa4f33
Thomas Christoffersen <t.christoffersen@creuna.no>
2018-06-19 14:56:23 +0200

    grid fixes

--- source/components/footer/footer.jsx
+++ source/components/footer/footer.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
 
+import Heading from '../../components/heading';
+
 const Footer = ({ className }) => (
-  <div className={cn('footer', className)}>footer</div>
+  <div className={cn('footer', className)}>
+    <div className="footer__logo">
+      <Heading level={2}>ABCD</Heading>
+    </div>
+    <div className="footer__info">extra information</div>
+  </div>
 );
 
 Footer.propTypes = {
   className: PropTypes.string
 };
+
 export default Footer;
