5590d6501464f1c228adaecd5c20cdf57d8cde63
Thomas Christoffersen <t.christoffersen@creuna.no>
2018-06-19 10:52:41 +0200

    theme for game

--- source/components/game/game.jsx
+++ source/components/game/game.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cn from 'classnames';
 
 import Link from '../../components/link';
 import Heading from '../../components/heading';
 import Image from '../../components/image';
 
-const Game = ({ title, href, imgSrc, isLoading }) => (
-  <div className="game">
+const themes = {
+  default: 'default'
+};
+
+const Game = ({ title, href, imgSrc, isLoading, theme }) => (
+  <div className={cn('game', `game--${theme}`)}>
     <Link routerHref={href}>
       <Image src={imgSrc} altText={title} className="game__image" />
       <Heading level={3} className="game__title">
         {title}
       </Heading>
-      {isLoading && <p>fetching</p>}
+      {isLoading && <p>fetching...</p>}
     </Link>
   </div>
 );
@@ -21,6 +26,14 @@ Game.propTypes = {
   title: PropTypes.string,
   isLoading: PropTypes.bool,
   href: PropTypes.string.isRequired,
-  imgSrc: PropTypes.string.isRequired
+  imgSrc: PropTypes.string.isRequired,
+  theme: PropTypes.oneOf(Object.keys(themes).map(key => themes[key]))
 };
+
+Game.defaultProps = {
+  theme: themes.default
+};
+
+Game.themes = themes;
+
 export default Game;
