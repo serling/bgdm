e5253863298dc51e1e0a70d3bf16bd287d997440
Thomas Christoffersen <t.christoffersen@creuna.no>
2018-06-25 14:52:03 +0200

    text in mockup

--- source/mockup/pages/index-page/index-page.jsx
+++ source/mockup/pages/index-page/index-page.jsx
@@ -21,7 +21,7 @@ const IndexPage = () => (
         quoteText="hello"
         gameTitle="asd"
         gameHref="#"
-        text="hello, everybody"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         reviewerName="Terje"
         reviewerHref="#"
       />
