9cc5a30ce36fab0d2864339b66757f02a77accd5
Thomas Christoffersen <t.christoffersen@creuna.no>
2018-07-06 13:19:21 +0200

    stateless filtered-games

--- source/components/filtered-games-list/filtered-games-list.jsx
+++ source/components/filtered-games-list/filtered-games-list.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import Heading from '../../components/heading';
 import GamesList from '../../components/games-list';
 import Button from '../../components/button';
@@ -8,85 +7,88 @@ import LoadingSpinner from '../../components/loading-spinner';
 import FilterControls from '../../components/filter-controls';
 import Text from '../../components/text';
 
-class FilteredGamesList extends React.Component {
-  static propTypes = {
-    heading: PropTypes.string,
-    lead: PropTypes.string,
-    showControls: PropTypes.bool,
-    showLoadMore: PropTypes.bool,
-    gridColumns: PropTypes.number,
-    gameTheme: PropTypes.string,
-    showNumerOfResults: PropTypes.bool,
-    isFetching: PropTypes.bool,
-    collection: PropTypes.array,
-    numberOfResults: PropTypes.number,
-    onClickLoadMore: PropTypes.func,
-    onClickFilterBy: PropTypes.func,
-    onClickOrderBy: PropTypes.func
-    //TODO: another abstraction -> heading, controls, buttons, grid
-  };
-
-  static defaultProps = {
-    collection: []
-  };
-
-  state = {};
-
-  render() {
-    return (
-      <div className="filtered-games-list">
-        <div className="filtered-games-list__header">
-          <Heading
-            level={2}
-            theme={Heading.themes.linethrough}
-            className="filtered-games-list__heading"
-          >
-            {this.props.heading}
-            {this.props.showNumerOfResults && (
-              <span> ({this.props.numberOfResults})</span>
-            )}
-          </Heading>
-          <Text
-            size={Text.sizes.medium}
-            text={this.props.lead}
-            className="filtered-games-list__lead"
-          />
-        </div>
-        {this.props.showControls && (
-          <FilterControls
-            disableControls={this.props.isFetching}
-            onClickFilterBy={this.props.onClickFilterBy}
-            onClickOrderBy={this.props.onClickOrderBy}
-          />
-        )}
-        <div className="filtered-games-list__message">
-          {this.props.numberOfResults === 0 &&
-            !this.props.isFetching && <span>No matches found</span>}
-        </div>
-        <div className="filtered-games-list__results">
-          {this.props.isFetching && (
-            <div className="filtered-games-list__loading">
-              <LoadingSpinner className="filtered-games-list__spinner" />
-            </div>
-          )}
-          <GamesList
-            heading={this.props.heading}
-            games={this.props.collection}
-            gridColumns={this.props.gridColumns}
-            gameTheme={this.props.gameTheme}
-          />
-          {this.props.showLoadMore && (
-            <Button
-              disabled={this.props.isFetching}
-              onClick={this.props.onClickLoadMore}
-            >
-              More
-            </Button>
-          )}
-        </div>
+const FilteredGamesList = ({
+  heading,
+  lead,
+  showControls,
+  showLoadMore,
+  gridColumns,
+  gameTheme,
+  showNumerOfResults,
+  isFetching,
+  collection,
+  numberOfResults,
+  onClickLoadMore,
+  onClickFilterBy,
+  onClickOrderBy
+}) => {
+  return (
+    <div className="filtered-games-list">
+      <div className="filtered-games-list__header">
+        <Heading
+          level={2}
+          theme={Heading.themes.linethrough}
+          className="filtered-games-list__heading"
+        >
+          {heading}
+          {showNumerOfResults && <span> ({numberOfResults})</span>}
+        </Heading>
+        <Text
+          size={Text.sizes.medium}
+          text={lead}
+          className="filtered-games-list__lead"
+        />
       </div>
-    );
-  }
-}
+      {showControls && (
+        <FilterControls
+          disableControls={isFetching}
+          onClickFilterBy={onClickFilterBy}
+          onClickOrderBy={onClickOrderBy}
+        />
+      )}
+      <div className="filtered-games-list__message">
+        {numberOfResults === 0 && !isFetching && <span>No matches found</span>}
+      </div>
+      <div className="filtered-games-list__results">
+        {isFetching && (
+          <div className="filtered-games-list__loading">
+            <LoadingSpinner className="filtered-games-list__spinner" />
+          </div>
+        )}
+        <GamesList
+          heading={heading}
+          games={collection}
+          gridColumns={gridColumns}
+          gameTheme={gameTheme}
+        />
+        {showLoadMore && (
+          <Button disabled={isFetching} onClick={onClickLoadMore}>
+            More
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+FilteredGamesList.propTypes = {
+  heading: PropTypes.string,
+  lead: PropTypes.string,
+  showControls: PropTypes.bool,
+  showLoadMore: PropTypes.bool,
+  gridColumns: PropTypes.number,
+  gameTheme: PropTypes.string,
+  showNumerOfResults: PropTypes.bool,
+  isFetching: PropTypes.bool,
+  collection: PropTypes.array,
+  numberOfResults: PropTypes.number,
+  onClickLoadMore: PropTypes.func,
+  onClickFilterBy: PropTypes.func,
+  onClickOrderBy: PropTypes.func //TODO: another abstraction -> heading, controls, buttons, grid
+};
+
+FilteredGamesList.defaultProps = {
+  collection: []
+};
 
 export default FilteredGamesList;
